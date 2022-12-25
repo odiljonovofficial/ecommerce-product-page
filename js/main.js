0aa02524e1662587058c79bfa158b465174c3c8c
odiljonov <odiljonovbe@gmail.com>
2022-12-25 17:21:06 +0500

    add img-showcase__control

--- js/main.js
+++ js/main.js
@@ -1,38 +1,49 @@
-// Object
 const modifiers = {
+  siteHeaderCartModalOpen: "site-header__cart-modal--open",
   imgThumbnailActive: "img-showcase__thumbnail--active",
+  lightboxOpen: "lightbox--open",
 };
 
-// SHOPPING CART MODAl
-window.addEventListener("DOMContentLoaded", () => {
-  const siteHeaderCartLink = document.querySelector(
-      ".js-site-header-cart-link"
-    ),
-    siteHeaderCartModal = document.querySelector(".site-header__cart-modal");
+// Shopping cart modal
+const elSiteHeaderCartLink = document.querySelector(
+  ".js-site-header-cart-link"
+);
+const elSiteHeaderCartModal = document.querySelector(
+  ".site-header__cart-modal"
+);
 
-  if (siteHeaderCartLink) {
-    siteHeaderCartLink.addEventListener("click", function (evt) {
-      evt.preventDefault();
+if (elSiteHeaderCartLink) {
+  elSiteHeaderCartLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
 
-      siteHeaderCartModal.classList.toggle("site-header__cart-modal--open");
+    elSiteHeaderCartModal.classList.toggle(modifiers.siteHeaderCartModalOpen);
+  });
+}
+
+// Image showcase
+const elProductPageGallery = document.querySelector(".product-page__gallery");
+const elImgShowcaseActiveImg = elProductPageGallery.querySelector(
+  ".img-showcase__active-img"
+);
+const elsImgShowcaseThumbnailButton = elProductPageGallery.querySelectorAll(
+  ".js-img-showcase-thumbnail-button"
+);
+const elsImgThumbnail = elProductPageGallery.querySelectorAll(
+  ".img-showcase__thumbnail"
+);
+
+elsImgShowcaseThumbnailButton.forEach(function (elButton) {
+  elButton.addEventListener("click", function () {
+    // Remove active state from all buttons
+    elsImgThumbnail.forEach(function (elImgThumbnail) {
+      elImgThumbnail.classList.remove(modifiers.imgThumbnailActive);
     });
-  }
 
-  // IMG SHOWCASE
-  const elsImgShowcaseThumbnailButton = document.querySelectorAll(
-    ".js-img-showcase-thumbnail-button"
-  );
-  const elsImgThumbnail = document.querySelectorAll(".img-showcase__thumbnail");
+    // Add active state to clicked button
+    elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
 
-  elsImgShowcaseThumbnailButton.forEach(function (elButton) {
-    elButton.addEventListener("click", function () {
-      // Remove active state from all buttons
-      elsImgThumbnail.forEach(function (elImgThumbnail) {
-        elImgThumbnail.classList.remove(modifiers.imgThumbnailActive);
-      });
-
-      // Add active state to clicked button
-      elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
-    });
+    // Update active image src accordingly
+    elImgShowcaseActiveImg.src = elButton.dataset.imgShowcaseBig;
+    elImgShowcaseActiveImg.srcset = `${elButton.dataset.imgShowcaseBig} 1x, ${elButton.dataset.imgShowcaseRetina} 2x`;
   });
 });
